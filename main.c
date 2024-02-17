35d02bb355bd18f6f7e638fd03f03d2aafe7243f
Deskordas <ds2007start@gmail.com>
2024-02-17 17:20:54 +0100

    Ncurses included!

--- main.c
+++ main.c
@@ -1,7 +1,80 @@
+#include <ncurses.h> //basic lib for pg interface 
+
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> // basic std
+#include <unistd.h>
+
+#include <time.h>
+
+char map[10][10];
+bool run;
+
+long frame_count;
+
+char c;
+
+void delay(unsigned ms) {
+	clock_t pause, start;
+
+	pause = ms * (CLOCKS_PER_SEC / 1000);
+	start = clock();
+	while( (clock() - start) < pause );
+}
+
+void mapInit(char filling_ch) {
+	for (int i = 0; i < 10; i++) {
+		for (int f = 0; f < 10; f++) {
+			map[i][f] = filling_ch;
+		}
+	}
+}
+
+void mapDraw() {
+	printw("\n");
+	for (int i = 0; i < 10; i++) {
+		for (int f = 0; f < 10; f++) {
+			printw("%c", map[i][f]);
+		}
+		printw("\n");
+	}
+}
+
+void start() {
+	initscr();
+	mapInit('.');
+	frame_count = 0;
+	run = true;
+}
+
+void gameLoop() {
+	while(run) {
+		frame_count++;
+
+		// clear an old frame
+		move(0,0);
+		
+		// draw a new frame
+		printw("Hello, World!%d", frame_count);
+		mapDraw();
+
+		// flip
+		refresh();
+
+		delay(30);
+
+	}
+}
+
+void stop() {
+	run = false;
+	endwin();
+}
 
 int main (int argc, char *argv[]) {
-	printf("Hello, DSPong_cli");
+	start();
+	
+	gameLoop();
+
+	stop();
 	return 0;
 }
